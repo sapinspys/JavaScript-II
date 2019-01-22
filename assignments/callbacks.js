888d0b65c7b8a90270ba1482d180dcac3f8978b4
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-22 15:08:46 -0500

    Completed task 2, callback.js, challenges + stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,25 +23,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {console.log(length)})
+
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(lastItem) {console.log(lastItem)})
+
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y)
 }
+
+sumNums(2, 3, function(add) {console.log(add)})
+
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y)
 }
 
+multiplyNums(2, 4, function(multiply) {console.log(multiply)})
+
+
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item))
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
 }
+
+contains('Pencil', items, function(compare) {console.log(compare)})
+
 
 /* STRETCH PROBLEM */
 
@@ -49,4 +65,7 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb(Array.from(new Set(array)))
 }
+
+removeDuplicates([1,2,3,1,2,3,4,4,5,6,7,7,7,7], function(x) {console.log(x)})
